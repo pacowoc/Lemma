af901db2b554e9811061a9acef0527bb738850b2
pacowoc <pacowoc@gmail.com>
2026-01-22 01:03:18 +0800

    New Mode Toggle at e200 rho

--- chronos.js
+++ chronos.js
@@ -3,7 +3,7 @@ import { Localization } from "../api/Localization";
 import { parseBigNumber, BigNumber } from "../api/BigNumber";
 import { theory } from "../api/Theory";
 import { Utils } from "../api/Utils";
-import { FreeCost } from "./api/Costs";
+import { ConstantCost, FreeCost } from "./api/Costs";
 
 var id = "chronos";
 var name = "Chronos";
@@ -13,7 +13,6 @@ var version = 1;
 var alwaysShowRefundButtons = () => true;
 var Exponent = BigNumber.ZERO;
 var t = BigNumber.ZERO;
-var maxLevel = 1;
 var realExponent = BigNumber.ZERO;
 var kDecreaseP = ui.createPopup({
     title: "What are you doing?",
@@ -44,7 +43,6 @@ var init = () => {
         a.getInfo = (amount) => Utils.getMathTo(getDesc(a.level), getDesc(a.level + amount));
         a.boughtOrRefunded = (_) => {
             theory.invalidateSecondaryEquation();
-            theory.invalidateTertiaryEquation();
             updateExponent();
         }
         a.maxLevel = 9;
@@ -57,7 +55,6 @@ var init = () => {
         b.getInfo = (amount) => Utils.getMathTo(getDesc(b.level), getDesc(b.level + amount));
         b.boughtOrRefunded = (_) => {
             theory.invalidateSecondaryEquation();
-            theory.invalidateTertiaryEquation();
             updateExponent();
         }
         b.maxLevel = 9;
@@ -74,9 +71,21 @@ var init = () => {
             }
             realExponent = Exponent;
             realize.level = 0;
-            theory.invalidatePrimaryEquation();
             t = BigNumber.from(0.01);
         }
+    }
+    //Mode Switch
+    {
+        sw = theory.createUpgrade(3,currency, new ConstantCost(BigNumber.from("1e200")));
+        sw.getDescription = (_) =>{
+            return "Change the Equation for k to mode B"
+        }
+        sw.getInfo = (_) =>"Toggle the mode for k Derivation"
+        sw.boughtOrRefunded = (_) =>{
+            theory.invalidateSecondaryEquation();
+            updateExponent();
+        }
+        sw.maxLevel = 1;
     }
     t = BigNumber.from(0.01);
     realExponent=BigNumber.ZERO;
@@ -85,16 +94,32 @@ var init = () => {
 }
 
 function updateExponent(){
-    let cValue = c.level+1;
-    let aValue = a.level+1;
-    let bValue = b.level+1;
-    let exp = 0;
-    for(i=1;i<=aValue;i++){
-        for(j=1;j<=bValue;j++){
-            exp+=1/Math.pow(1+i,j)*Math.sin(cValue*Math.pow(1+i,j))
+    if(sw.level==0){
+        let cValue = c.level+1;
+        let aValue = a.level+1;
+        let bValue = b.level+1;
+        let tmp = 0;
+        for(i=1;i<=aValue;i++){
+            for(j=1;j<=bValue;j++){
+                tmp+=1/Math.pow(1+i,j)*Math.sin(cValue*Math.pow(1+i,j))
+            }
         }
+        Exponent = BigNumber.from(tmp*cValue/100);
     }
-    Exponent = BigNumber.from(exp*cValue/100);
+    else if(sw.level==1){
+        let cValue = c.level+1;
+        let aValue = a.level+1;
+        let bValue = b.level+1;
+        let k1 = 0;
+        let k2 = 0;
+        for(i=1;i<=aValue;i++){
+            for(j=1;j<=bValue;j++){
+                k1+=1/Math.pow(1+i,j)*Math.sin(cValue*Math.pow(1+i,j))
+                k2+=1/Math.pow(1+i,j)*Math.cos(cValue*Math.pow(1+i,j))
+            }
+        }
+        Exponent = BigNumber.from(Math.sqrt(k1*k1+k2*k2)*cValue/100/Math.sqrt(2));
+    }
 }
 
 var tick = (elapsedTime, multiplier) => {
@@ -115,14 +140,17 @@ var setInternalState = (state) => {
 }
 
 var getPrimaryEquation = () => {
-    return "\\tau = \\max \\rho^2\\ \\ \\ \\ \\dot{\\rho}=t^{"+realExponent.toString(6)+"}";
+    return "\\tau = \\max \\rho^{0.6}\\ \\ \\ \\ \\dot{\\rho}=t^{"+realExponent.toString(6)+"}";
 }
 
-var getSecondaryEquation = () => "k=\\frac{c}{100}\\Sigma_{i=1}^a\\Sigma_{j=1}^b{\\frac{\\sin(c(1+i)^j)}{(1+i)^j}}";
-theory.secondaryEquationHeight = 50;
+var getSecondaryEquation = () => {
+    if(sw.level==0) return "\\begin{matrix}Mode:A\\\\k=\\frac{c}{100}\\Sigma_{i=1}^a\\Sigma_{j=1}^b{\\frac{\\sin(c(1+i)^j)}{(1+i)^j}}\\end{matrix}"
+    else return "\\begin{matrix}Mode:B\\\\k_1=\\Sigma_{i=1}^a\\Sigma_{j=1}^b{\\frac{\\sin(c(1+i)^j)}{(1+i)^j}}\\\\k_2=\\Sigma_{i=1}^a\\Sigma_{j=1}^b{\\frac{\\cos(c(1+i)^j)}{(1+i)^j}}\\\\k=\\frac{c}{100\\sqrt{2}}\\sqrt{k_1^2+k_2^2}\\end{matrix}"
+};
+theory.secondaryEquationHeight = 150;
 theory.secondaryEquationScale = 1.2;
 var getTertiaryEquation = () => "t="+t.toString(4) + "\\ " +"k="+Exponent.toString(6) + "\\ " + "\\dot{\\rho}=" + rhodot.toString(6);
-var getTau = () => currency.value.pow(2);
+var getTau = () => currency.value.pow(0.6);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 
 init();
